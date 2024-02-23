8d29157c55cb6fa3f3a16d2a6d48ba6efc6a012f
Daler Sharipov <148033893+shdaler@users.noreply.github.com>
2024-02-23 19:24:17 +0500

    Add files via upload

---
+++ gulp/config/ftp.js
@@ -0,0 +1,6 @@
+export let congfigFTP = {
+  host: "", //adres ftp servera
+  user: "", //imya pol'zovatelya
+  password: "", //parol'
+  parallel: 5, // kol-vo odnovremennikh potokov
+};
---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+import * as flsFunctions from "./modules/functions.js";
+
+flsFunctions.isWebp();
+
+import { Navigation, Pagination } from "swiper/modules";
+
+const swiper = new Swiper();
